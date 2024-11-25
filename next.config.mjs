621a35f69268da78ac42ca4478015b3ae4557b31
Brian Pillsbury <bpillsbury@edg.io>
2024-11-25 13:14:46 -0500

    Remove Edgio-related content from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,9 @@
-// This file was automatically added by edgio init.
-// You should commit this file to source control.
-// This file was automatically added by edgio init.
-// You should commit this file to source control.
-import { withEdgio } from "@edgio/next/config/index.js";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["assets-flightaware.bpillsbury.com"], // Add your external domain here
   },
+  reactStrictMode: true,
 };
 
-const _preEdgioExport = nextConfig;
-
-export default (_phase, _config) =>
-  withEdgio({
-    ..._preEdgioExport,
-  });
+export default nextConfig;
